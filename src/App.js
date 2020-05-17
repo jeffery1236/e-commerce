f0c736ccc36e062ad25cf9ade5395c49121fe76f
JEFF0019 <JEFF0019@e.ntu.edu.sg>
2020-05-16 23:28:35 -0400

    Added shop page rendering shop data

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Route, Switch } from "react-router-dom";
 import "./App.css";
 
 import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from './pages/shop/shop.component';
 
 const Hats = () => <h1>Hello Hats</h1>;
 
@@ -12,7 +13,7 @@ function App() {
     <div className="App">
       <Switch>  {/* Switch ensures only 1 route is matched */}
         <Route exact path="/" component={HomePage} />
-        <Route exact path="/shop/hats" component={Hats} />
+        <Route exact path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
